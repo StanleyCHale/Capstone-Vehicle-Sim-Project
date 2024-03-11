c5f0af783e44681874ac322c17d8ff04f61039be
Stanley Hale <106855723+StanTheMudkip@users.noreply.github.com>
2024-03-10 21:09:02 -0700

    Created Menu Layout, WIP
    Need to modify Buttons so that they spawn with an action enumerator so we can handle each button separately.
    
    It currently crashes when we insert the enumerator...

--- src/car/src/main_menu.rs
+++ src/car/src/main_menu.rs
@@ -1,3 +1,4 @@
+use bevy::app::AppExit;
 use bevy::prelude::Resource;
 //This is a bevy plugin for the main menu of the game
 
@@ -8,8 +9,19 @@ pub struct MainMenuPlugin;
 impl Plugin for MainMenuPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, main_menu_setup)
-            .add_systems(Update, handle_start_button);
+            .add_systems(Update, handle_menu_buttons);
     }
+}
+
+// All actions that can be triggered from a button click
+#[derive(Component)]
+enum MenuButtonAction {
+    Play,
+    Settings,
+    SettingsSound,
+    BackToMainMenu,
+    BackToSettings,
+    Quit,
 }
 
 
@@ -25,48 +37,165 @@ fn main_menu_setup(mut commands: Commands, asset_server: Res<AssetServer>,) {
     //Spawn a camera for our 2d bundle
     //commands.spawn(UiCameraConfig::default());
 
-    //Spawn a button bundle for the Start Game button
-    commands.spawn(ButtonBundle {
+    //This is a node bundle that will be the parent of all of our UI elements
+    commands.spawn(NodeBundle {
         style: Style {
-            align_self: AlignSelf::Center,
-            align_content: AlignContent::Center,
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            // Place children in a column
+            flex_direction: FlexDirection::Column,
+            // Center children
+            align_items: AlignItems::Center,
             justify_content: JustifyContent::Center,
-            margin: UiRect::all(Val::Auto),
-            ..Default::default()
+            ..default()
         },
-        background_color: BackgroundColor(Color::NONE),
-        ..Default::default()
-    })
-    .with_children(|parent| {
-        parent.spawn(ImageBundle {
+        background_color: BackgroundColor(Color::ALICE_BLUE),
+        ..default()
+    }).with_children(|parent| {
+
+        //Spawn a button bundle for the Start Game button
+        parent.spawn(ButtonBundle {
             style: Style {
-                max_width: Val::Percent(20.0),
-                max_height: Val::Percent(10.0),
-                margin: UiRect::all(Val::Auto),
+                align_self: AlignSelf::Center,
+                align_content: AlignContent::Center,
                 justify_content: JustifyContent::Center,
-                align_items: AlignItems::Center,
+                margin: UiRect::all(Val::Px(20.0)),
+                min_width: Val::Vw(20.0),
+                min_height: Val::Vh(6.0),
                 ..Default::default()
             },
-            image: ui_assets.button.clone().into(),
+            background_color: BackgroundColor(Color::NONE),
             ..Default::default()
         })
-        .insert(FocusPolicy::Pass)
         .with_children(|parent| {
-            parent.spawn(TextBundle {
-                text: Text::from_section(
-                    "Start Game", 
-                    TextStyle {
-                        font_size: 40.0,
-                        color: Color::WHITE,
-                        ..Default::default()
-                    },
-                ),
-                focus_policy: FocusPolicy::Pass,
+            //parent.spawn(MenuButtonAction::Play);
+            parent.spawn(ImageBundle {
+                style: Style {
+                    max_width: Val::Percent(100.0),
+                    max_height: Val::Percent(100.0),
+                    margin: UiRect::all(Val::Percent(0.0)),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..Default::default()
+                },
+                image: ui_assets.button.clone().into(),
                 ..Default::default()
+            })
+            .insert(FocusPolicy::Pass)
+            .with_children(|parent| {
+                parent.spawn(TextBundle {
+                    text: Text::from_section(
+                        "Start Game", 
+                        TextStyle {
+                            font_size: 40.0,
+                            color: Color::WHITE,
+                            ..Default::default()
+                        },
+                    ),
+                    focus_policy: FocusPolicy::Pass,
+                    ..Default::default()
+                });
             });
+        
         });
-    
-    });
+
+        //Spawn a button bundle for the Settings button
+        parent.spawn(ButtonBundle {
+            style: Style {
+                align_self: AlignSelf::Center,
+                align_content: AlignContent::Center,
+                justify_content: JustifyContent::Center,
+                margin: UiRect::all(Val::Px(20.0)),
+                min_width: Val::Vw(20.0),
+                min_height: Val::Vh(6.0),
+                ..Default::default()
+            },
+            background_color: BackgroundColor(Color::NONE),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent.spawn(ImageBundle {
+                style: Style {
+                    max_width: Val::Percent(100.0),
+                    max_height: Val::Percent(100.0),
+                    margin: UiRect::all(Val::Percent(0.0)),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..Default::default()
+                },
+                image: ui_assets.button.clone().into(),
+                ..Default::default()
+            })
+            .insert(FocusPolicy::Pass)
+            .with_children(|parent| {
+                //parent.spawn(MenuButtonAction::Settings);
+                parent.spawn(TextBundle {
+                    text: Text::from_section(
+                        "Settings", 
+                        TextStyle {
+                            font_size: 40.0,
+                            color: Color::WHITE,
+                            ..Default::default()
+                        },
+                    ),
+                    focus_policy: FocusPolicy::Pass,
+                    ..Default::default()
+                });
+            });
+        
+        });
+
+        //Spawn a button bundle for the Exit button
+        parent.spawn((
+            ButtonBundle {
+            style: Style {
+                align_self: AlignSelf::Center,
+                align_content: AlignContent::Center,
+                justify_content: JustifyContent::Center,
+                margin: UiRect::all(Val::Px(20.0)),
+                min_width: Val::Vw(20.0),
+                min_height: Val::Vh(6.0),
+                ..Default::default()
+            },
+            background_color: BackgroundColor(Color::NONE),
+            ..Default::default()
+            },
+            //MenuButtonAction::Quit,
+        ))
+        //
+        .with_children(|parent| {
+            parent.spawn(ImageBundle {
+                style: Style {
+                    max_width: Val::Percent(100.0),
+                    max_height: Val::Percent(100.0),
+                    margin: UiRect::all(Val::Percent(0.0)),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
+                    ..Default::default()
+                },
+                image: ui_assets.button.clone().into(),
+                ..Default::default()
+            })
+            .insert(FocusPolicy::Pass)
+            //
+            .with_children(|parent| {
+                parent.spawn(TextBundle {
+                    text: Text::from_section(
+                        "Quit Game", 
+                        TextStyle {
+                            font_size: 40.0,
+                            color: Color::WHITE,
+                            ..Default::default()
+                        },
+                    ),
+                    focus_policy: FocusPolicy::Pass,
+                    ..Default::default()
+                });
+            });
+            //
+        });
+        //
+    }); 
 
     //Insert our UI resource
     commands.insert_resource(ui_assets);
@@ -83,39 +212,92 @@ struct UiAssets {
 impl Resource for UiAssets {}
 
 //Runs when the start button is pushed
-fn handle_start_button(
+//Currently handles all of the buttons atm//
+fn handle_menu_buttons(
     mut commands: Commands, 
-    interaction_query: Query<(&Children, &Interaction), Changed<Interaction>>,
+    interaction_query: Query<(&Children, &Interaction, &MenuButtonAction), (Changed<Interaction>, With<Button>)>,
+    interaction_query2: Query<(&Children, &Interaction), Changed<Interaction>>,
+    mut background_query: Query<&mut BackgroundColor>,
     mut image_query: Query<&mut UiImage>,
     ui_assests: Res<UiAssets>,
-    button_query: Query<Entity, With<Button>>
+    button_query: Query<Entity, With<Button>>,
+    mut app_exit_events: EventWriter<AppExit>,
 ) {
-    for (children, interaction) in interaction_query.iter() {
-        let child = children.iter().next().unwrap();
-        let mut image = image_query.get_mut(*child).unwrap();
+    // 
+        //
+        for (children, interaction, menu_button_action) in &interaction_query {
+            let child = children.iter().next().unwrap();
+            let mut image = image_query.get_mut(*child).unwrap();
+            println!("A Button was clicked!");
 
-        match interaction {
-            Interaction::Pressed => {
-                println!("Start Game Button Clicked");
+            //
+            match menu_button_action {
+                MenuButtonAction::Quit => {
+                    image.texture = ui_assests.button_pressed.clone();
+                    println!("Quit Button Clicked");
+                    app_exit_events.send(AppExit);
+                }
+                MenuButtonAction::Play => {
+                    image.texture = ui_assests.button_pressed.clone();
+                    println!("Play Game Button Clicked");
 
-                //Change the image to the pressed image
-                image.texture = ui_assests.button_pressed.clone();
+                    //set background colour to none
+                    background_query.iter_mut().for_each(|mut background| {
+                        background.0 = Color::NONE;
+                    });
 
-                //Need to do all of the setup for starting the game here
-
-                //despawn the menu
-                button_query.iter().for_each(|entity| {
-                    commands.entity(entity).despawn_recursive();
-                });
-
-                //commands.spawn_bundle(StartGameBundle);
+                    //despawn the menu
+                    button_query.iter().for_each(|entity| {
+                        commands.entity(entity).despawn_recursive();
+                    });
+                }
+                MenuButtonAction::Settings => println!("Settings Button Clicked"),
+                MenuButtonAction::SettingsSound => println!("Sound Button Clicked"),
+                MenuButtonAction::BackToMainMenu => println!("Back to Main Menu Button Clicked"),
+                MenuButtonAction::BackToSettings => println!("Back to Settings Button Clicked"),
             }
-            Interaction::Hovered => {
-                println!("Start Game Button Hovered");
-            }
-            _ => {}
+            //
         }
-    }
+        //
+
+        //
+        for (children, interaction) in &interaction_query2 {
+            let child = children.iter().next().unwrap();
+            let mut image = image_query.get_mut(*child).unwrap();
+            //
+            match interaction {
+                Interaction::Pressed => {
+                    println!("Button Clicked");
+
+                    //Change the image to the pressed image
+                    image.texture = ui_assests.button_pressed.clone();
+
+                    //Need to do all of the setup for starting the game here
+
+                    //despawn the menu
+                    button_query.iter().for_each(|entity| {
+                        commands.entity(entity).despawn_recursive();
+                    });
+
+                    //set background colour to none
+                    background_query.iter_mut().for_each(|mut background| {
+                        background.0 = Color::NONE;
+                    });
+
+                    //Exit the app
+                    //app_exit_events.send(AppExit);
+
+                    //commands.spawn_bundle(StartGameBundle);
+                }
+                Interaction::Hovered => {
+                    println!("Button Hovered");
+                }
+                _ => {}
+            }
+            //
+        }
+        //
+        
 }
 
 //Function to despawn the main menu
