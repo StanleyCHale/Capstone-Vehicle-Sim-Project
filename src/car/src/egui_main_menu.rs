a4261b775d1cb5d713c833c0882b1ce8f9a379c4
Gregg H. Windows 10 <greggory.png@gmail.com>
2024-05-13 18:08:46 -0700

    Still bad but not quite as bad

--- src/car/src/egui_main_menu.rs
+++ src/car/src/egui_main_menu.rs
@@ -1,7 +1,8 @@
+#![allow(dead_code)]
 use bevy::prelude::*;
-use bevy_egui::{egui::{self, Ui}, EguiContexts};
+use bevy_egui::{egui, EguiContexts};
 
-// The following code adapted from the bevy_egui examples found here https://github.com/mvlabat/bevy_egui/blob/main/examples/ui.rs and here: https://github.com/emilk/egui/blob/master/crates/egui_demo_lib/src/demo/widget_gallery.rs
+// The following code adapted from the bevy_egui examples found here https://github.com/mvlabat/bevy_egui/blob/main/examples/ui.rs and here https://github.com/emilk/egui/blob/master/crates/egui_demo_lib/src/demo/widget_gallery.rs
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 pub struct MainMenu {
     enabled: bool,
@@ -46,15 +47,15 @@ impl MainMenu {
 
     fn show(&mut self, ctx: &egui::Context) {
         egui::Window::new("Driver's Altitude")
-                .resizable(true)
-                .default_width(280.0 * 5.)
-                .default_height(280.0 * 2.)
+                .resizable(false)
+                .collapsible(false)
+                .movable(false)
                 .show(ctx, |ui| {
-                    self.ui(ui);
+                    self.ui(ui, ctx);
                 });
     }
 
-    fn ui(&mut self, ui: &mut egui::Ui) {
+    fn ui(&mut self, ui: &mut egui::Ui, ctx: &egui::Context) {
         ui.add_enabled_ui(self.enabled, |ui| {
             ui.set_visible(self.visible);
 
@@ -68,15 +69,35 @@ impl MainMenu {
         });
 
         ui.separator();
+
+        let screen_width = ctx.input(|i| i.screen_rect().width());
+        let screen_height = ctx.input(|i| i.screen_rect().height());
+
+        ui.set_min_width(screen_width);
+        ui.set_min_height(screen_height);
+        ui.style_mut().wrap = Some(false);
     }
 }
 
-pub fn egui_main_menu(mut contexts: EguiContexts) {
+pub fn egui_main_menu(mut commands: Commands, mut contexts: EguiContexts) {
+    
     let mut main_menu_struct = MainMenu {
         enabled: true,
         visible: true,
         opacity: 1.0
     };
+
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            ..default()
+        });
 
     let ctx = contexts.ctx_mut();
 
