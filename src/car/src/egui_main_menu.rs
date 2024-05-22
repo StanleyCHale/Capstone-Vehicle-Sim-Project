fdbd00828db8b03b6212e9be564f902f6fc84582
Stanley Hale <halesta@oregonstate.edu>
2024-05-22 15:08:26 -0700

    Added Settings Menu
    Renamed:
    gallery_grid_contents -> gallery_main_contents
    
    Added new function:
    gallery_settings_contents

--- src/car/src/egui_main_menu.rs
+++ src/car/src/egui_main_menu.rs
@@ -33,14 +33,16 @@ enum MenuState {
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
 #[derive(Resource)]
 pub struct MainMenu {
-    enabled: bool,
+    main: bool,
+    settings: bool,
     visible: bool,
 }
 
 impl Default for MainMenu {
     fn default() -> Self {
         Self {
-            enabled: true,
+            main: true,
+            settings: false,
             visible: true,
         }
     }
@@ -48,22 +50,26 @@ impl Default for MainMenu {
 
 impl MainMenu {
     fn show(&mut self, ctx: &egui::Context, app_exit_events: EventWriter<AppExit>, game_state: ResMut<NextState<GameState>>,) {
-        if self.enabled {
-            self.gallery_grid_contents(ctx, app_exit_events, game_state);
+        if self.main {
+            self.gallery_main_contents(ctx, app_exit_events, game_state);
+        }
+        if self.settings {
+            self.gallery_settings_contents(ctx);
         }
     }
 
     /*
      * This function generates the actual UI of the main menu. Any UI element rearranging should be done here.
      */
-    fn gallery_grid_contents(
+    fn gallery_main_contents(
         &mut self,
         ctx: &egui::Context,
         app_exit_events: EventWriter<AppExit>,
         mut game_state: ResMut<NextState<GameState>>,
     ) {
         let Self {
-            enabled: _,
+            main: _,
+            settings: _,
             visible: _,
         } = self;
 
@@ -101,7 +107,7 @@ impl MainMenu {
                     //Transition to the "In Game" state
                     game_state.set(GameState::InGame);
                     
-                    self.enabled = false;
+                    self.main = false;
                 }
 
                 ui.add_space(10.0); // Space between buttons
@@ -111,7 +117,7 @@ impl MainMenu {
                     .add_sized([200.0, 50.0], egui::Button::new("Settings"))
                     .clicked()
                 {
-                    println!("egui: Settings Clicked");
+                    self.settings = true;
                 }
 
                 ui.add_space(10.0); // Space between buttons
@@ -122,6 +128,92 @@ impl MainMenu {
                     .clicked()
                 {
                     exit_program(app_exit_events);
+                }
+
+                ui.end_row();
+            });
+        });
+    }
+
+
+    /*
+     * This function generates the actual UI of the main menu. Any UI element rearranging should be done here.
+     */
+    fn gallery_settings_contents(
+        &mut self,
+        ctx: &egui::Context,
+    ) {
+        let Self {
+            main: _,
+            settings: _,
+            visible: _,
+        } = self;
+
+        
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.vertical_centered(|ui| {     // Center the UI
+
+                // Modify text size
+                let mut custom_style = egui::Style::default();
+
+                custom_style.text_styles = [
+                    (egui::TextStyle::Heading, egui::FontId::proportional(64.0)),
+                    (egui::TextStyle::Body, egui::FontId::proportional(32.0)),
+                    (egui::TextStyle::Button, egui::FontId::proportional(18.0)),
+                    (egui::TextStyle::Small, egui::FontId::proportional(14.0)),
+                ]
+                .into();
+
+                ctx.set_style(custom_style);
+
+                // Start of UI elements
+                ui.heading("Driver's Altitude");
+
+                ui.add_space(60.0); // Space between header and label
+
+                ui.add(egui::Label::new("Settings Menu"));
+
+                ui.add_space(20.0); // Space between label and buttons
+                ui.end_row();
+
+                if ui
+                    .add_sized([200.0, 50.0], egui::Button::new("Audio Settings"))
+                    .clicked()
+                {
+                    
+                    self.main = false;
+                    self.settings = true;
+                }
+
+                ui.add_space(10.0); // Space between buttons
+                ui.end_row();
+
+                if ui
+                    .add_sized([200.0, 50.0], egui::Button::new("Vehicle Settings"))
+                    .clicked()
+                {
+                    println!("egui: Settings Clicked");
+                }
+
+                ui.add_space(10.0); // Space between buttons
+                ui.end_row();
+
+                if ui
+                    .add_sized([200.0, 50.0], egui::Button::new("Terrain Settings"))
+                    .clicked()
+                {
+                    println!("egui: Settings Clicked");
+                }
+
+                ui.add_space(10.0); // Space between buttons
+                ui.end_row();
+
+                if ui
+                    .add_sized([200.0, 50.0], egui::Button::new("Back"))
+                    .clicked()
+                {
+                    self.settings = false;
+                    self.main = true;
                 }
 
                 ui.end_row();
